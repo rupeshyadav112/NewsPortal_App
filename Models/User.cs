b5de492fdd8c13c4311ba3aaaec5496500a2b597
ArjunMahato1 <amahato992@rku.ac.in>
2025-02-26 22:42:18 +0530

    Users model added

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsPortal_App.Models
+{
+    public class User
+    {
+        [Key] // Primary key specify karein
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfileImagePath { get; set; } = "~/images/avatar.png";
+        public string? FullName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string? GoogleUserId { get; set; }
+        public bool IsGoogleAccount { get; set; } = false;
+        // Navigation Properties
+
+        //public ICollection<Comment> Comments { get; set; } // A user can make many comments
+        //public ICollection<CommentLike> Likes { get; set; } // A user can like many comments
+
+    }
+}
