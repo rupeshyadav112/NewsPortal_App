1da747dfbc5757487a2c5f3d153922881e8334a0
rupeshyadav112 <135962952+rupeshyadav112@users.noreply.github.com>
2025-02-26 22:28:53 +0530

    Post Model Completed

---
+++ Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NewsPortal_App.Models
+{
+        public class Post
+        {
+            public int PostID { get; set; }
+
+            [Required]
+            [MaxLength(255)]
+            public string Title { get; set; }
+
+            [Required]
+            public string Content { get; set; }
+
+            [Required]
+            [MaxLength(50)]
+            public string Category { get; set; }
+
+            [MaxLength(255)]
+            public string ImagePath { get; set; }
+
+            [MaxLength(50)]
+            public string FontStyle { get; set; }
+
+            public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+            // Navigation Properties
+            //public ICollection<Comment> Comments { get; set; } // A post can have many comments
+        }
+    }
+
+
