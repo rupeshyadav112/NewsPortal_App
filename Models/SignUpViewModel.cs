64240f7db397219bd5c93a89a4af626c7caa3bde
krishnakumaryadav45 <ky8702385@gmail.com>
2025-02-28 21:17:28 +0530

    signupmodel added

---
+++ Models/SignUpViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsPortal_App.Models
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$",
+            ErrorMessage = "Username must be alphanumeric")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$",
+            ErrorMessage = "Password must contain uppercase, lowercase, and number")]
+        public string Password { get; set; }
+    }
+}
