4022fe99b8bf8bb069b5ac303cb2da36340a38b6
ArjunMahato1 <amahato992@rku.ac.in>
2025-03-01 13:18:42 +0530

    Commentslike model added do not need to change this

---
+++ Models/CommentLike.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsPortal_App.Models
+{
+    public class CommentLike
+    {
+        [Key]
+        public int LikeID { get; set; }
+        public int CommentID { get; set; } // Foreign key to Comment
+        public int UserID { get; set; } // Foreign key to User
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public Comment Comment { get; set; } // Each like belongs to one comment
+        public User User { get; set; } // Each like is made by one user
+    }
+}
