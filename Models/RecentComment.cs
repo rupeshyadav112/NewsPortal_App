473485a65b2044c19d18410e0f5ea49e27eda494
krishnakumaryadav45 <ky8702385@gmail.com>
2025-03-02 22:24:35 +0530

    Recentcommentmodeldone

---
+++ Models/RecentComment.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsPortal_App.Models
+{
+    public class RecentComment
+    {
+        public int CommentID { get; set; }
+        public string CommentText { get; set; }
+        public string Username { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
