ed716da06324e35bf736e223fab3490ccfdd2b3e
ArjunMahato1 <amahato992@rku.ac.in>
2025-03-02 21:50:09 +0530

    Recentpostmodel complete

---
+++ Models/RecentPost.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsPortal_App.Models
+{
+    public class RecentPost
+    {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string PostImage { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
