4dc55604244048ad062222accbead3fd6aad9be8
ArjunMahato1 <amahato992@rku.ac.in>
2025-03-02 21:48:04 +0530

    Recentuser model complete

---
+++ Models/RecentUser.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsPortal_App.Models
+{
+    public class RecentUser
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string UserImage { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
