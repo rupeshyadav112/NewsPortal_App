e5f78f3ac684857589a10bf8c10328d1000e401c
krishnakumaryadav45 <ky8702385@gmail.com>
2025-03-02 22:30:33 +0530

    articlemodeldone

---
+++ Models/Article.cs
@@ -0,0 +1,14 @@
+﻿namespace NewsPortal_App.Models
+{
+    public class Article
+    {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string Category { get; set; }
+        public string ImagePath { get; set; }
+        public string FontStyle { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
