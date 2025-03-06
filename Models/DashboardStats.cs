3b66b9c83359f79cab2e74e3d7a6c3a6fb5e7638
krishnakumaryadav45 <ky8702385@gmail.com>
2025-03-06 17:15:42 +0530

    dashboardmodeladded

---
+++ Models/DashboardStats.cs
@@ -0,0 +1,12 @@
+﻿namespace NewsPortal_App.Models
+{
+    public class DashboardStats
+    {
+        public int TotalUsers { get; set; }
+        public int TotalComments { get; set; }
+        public int TotalPosts { get; set; }
+        public int LastMonthUsers { get; set; }
+        public int LastMonthComments { get; set; }
+        public int LastMonthPosts { get; set; }
+    }
+}
